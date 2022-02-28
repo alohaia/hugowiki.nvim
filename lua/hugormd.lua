e5c83a3ca3b38babbc1677e3515da1c16db729ea
alohaia <liu.qihuan@outlook.com>
2022-02-28 22:29:53 +0800

    fix: RMarkdown kintting problems

--- lua/hugormd.lua
+++ lua/hugormd.lua
@@ -4,46 +4,51 @@ local configs = vim.g.hugowiki_rmd_auto_knit
 
 M.rmd_writepost = function()
     if M.hugowiki_rmd_knitting then
-        print("A knitting job is running.")
+        vim.notify("A knitting job is running.")
         return
     end
     M.hugowiki_rmd_knitting = true
 
-    local stdin = vim.loop.new_pipe(false)
     local stdout = vim.loop.new_pipe(false)
     local stderr = vim.loop.new_pipe(false)
-
-    local onread = vim.schedule_wrap(function(err, data)
-        if err then
-            print('ERROR: ', err)
-            -- TODO handle err
-        end
-        if data then
-            print(data)
-        end
-    end)
+    local results = {}
 
     local r_handle
     local filename = vim.fn.expand("%:p")
-    r_handle = vim.loop.spawn('Rscript', {
+    filename = string.gsub(filename, vim.fn.expand(vim.g.hugowiki_home) .. "/", "")
+    r_handle = vim.loop.spawn("Rscript", {
             args = {configs.r_script, filename, string.gsub(filename, "%.Rmd$", ".md")},
-            stdio = {stdin, stdout, stderr},
+            stdio = {nil, stdout, stderr},
             cwd = configs.cwd
         },
         vim.schedule_wrap(function(code, signal)
-            stdin:close()
             stdout:close()
-            stderr:close()
             r_handle:close()
+            vim.fn.setqflist({}, 'r', {
+                title = '[hugowiki.nvim]RMarkdown knitting output',
+                lines = results
+            })
             if code ~= 0 or signal ~= 0 then
-                print('exit with', code, signal)
+                vim.notify("Knitting failed, exit with code " .. code .. " and signal " .. signal, vim.log.levels.TRACE)
+            else
+                vim.notify("Knitting completed.", vim.log.levels.INFO)
             end
             M.hugowiki_rmd_knitting = false
         end)
     )
-
-    vim.loop.read_start(stdout, onread)
-    vim.loop.read_start(stderr, onread)
+    local read_start = vim.schedule_wrap(function(err, data)
+        -- assert(not err, err)
+        if err then
+            results = vim.fn.add(results, string.gsub(err, "%s+$", "") .. "\n")
+            vim.notify(err, vim.log.levels.ERROR)
+        end
+        if data then
+            results = vim.fn.add(results, string.gsub(data, "%s+$", "") .. "\n")
+            -- vim.notify(data, vim.log.levels.INFO)
+        end
+    end)
+    vim.loop.read_start(stdout, read_start)
+    vim.loop.read_start(stderr, read_start)
 end
 
 return M
